ead9c2ac7e375e37c1b2d1dc1ffb02771dd2a82a
David Ogilvy <github@thortek.com.au>
2015-04-18 22:10:16 +1000

    Major update
    One main spi_transaction function to handle any combination of:
    COMMAND, ADDRESS, MOSI, MISO, and DUMMY.
    
    Macro functions defined for reading and writing 8/16/32 bits.
    
    spi_txd and spi_rxd macros for sending or receiving any number of bits
    up to 32.

--- driver/spi.c
+++ driver/spi.c
@@ -26,10 +26,6 @@
 #include "driver/spi.h"
 
 
-
-
-
-
 ////////////////////////////////////////////////////////////////////////////////
 //
 // Function Name: spi_init
@@ -45,6 +41,7 @@ void spi_init(uint8 spi_no){
 	spi_init_gpio(spi_no, SPI_CLK_USE_DIV);
 	spi_clock(spi_no, SPI_CLK_PREDIV, SPI_CLK_CNTDIV);
 	spi_tx_byte_order(spi_no, SPI_BYTE_ORDER_HIGH_TO_LOW);
+	spi_rx_byte_order(spi_no, SPI_BYTE_ORDER_HIGH_TO_LOW); 
 
 	SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_CS_SETUP|SPI_CS_HOLD);
 	CLEAR_PERI_REG_MASK(SPI_USER(spi_no), SPI_FLASH_MODE);
@@ -152,104 +149,153 @@ void spi_tx_byte_order(uint8 spi_no, uint8 byte_order){
 		CLEAR_PERI_REG_MASK(SPI_USER(spi_no), SPI_WR_BYTE_ORDER);
 	}
 }
+////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Function Name: spi_txd
-//   Description: SPI transmit data up to 32bits of data
+// Function Name: spi_rx_byte_order
+//   Description: Setup the byte order for shifting data into buffer
 //    Parameters: spi_no - SPI (0) or HSPI (1)
-//				  no_bits - actual number of bits to transmit
-//				  data - actual data to transmit (assumes data is in lower bits)
+//				  byte_order - SPI_BYTE_ORDER_HIGH_TO_LOW (1) 
+//							   Data is read in starting with Bit31 and down to Bit0
+//
+//							   SPI_BYTE_ORDER_LOW_TO_HIGH (0)
+//							   Data is read in starting with the lowest BYTE, from 
+//							   MSB to LSB, followed by the second lowest BYTE, from
+//							   MSB to LSB, followed by the second highest BYTE, from
+//							   MSB to LSB, followed by the highest BYTE, from MSB to LSB
+//							   0xABCDEFGH would be read as 0xGHEFCDAB
+//
 //				 
 ////////////////////////////////////////////////////////////////////////////////
 
-void spi_txd(uint8 spi_no, uint8 no_bits, uint32 data ){
+void spi_rx_byte_order(uint8 spi_no, uint8 byte_order){
 
 	if(spi_no > 1) return;
 
-	uint8 extra_bits = no_bits%8;
+	if(byte_order){
+		SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_RD_BYTE_ORDER);
+	} else {
+		CLEAR_PERI_REG_MASK(SPI_USER(spi_no), SPI_RD_BYTE_ORDER);
+	}
+}
+////////////////////////////////////////////////////////////////////////////////
 
-	while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR);	
-	//enable MOSI
-	SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_MOSI);
-	//disable MISO, ADDR, COMMAND, DUMMY
-	CLEAR_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_MISO|SPI_USR_COMMAND|SPI_USR_ADDR|SPI_USR_DUMMY);
-	//setup MOSI bit length
-	WRITE_PERI_REG(SPI_USER1(spi_no), ((no_bits-1)&SPI_USR_MOSI_BITLEN)<<SPI_USR_MOSI_BITLEN_S); //set bits in MOSI BITLEN. Clears all ADDR/MISO/DUMMY BITLENs!
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name: spi_transaction
+//   Description: SPI transaction function
+//    Parameters: spi_no - SPI (0) or HSPI (1)
+//				  cmd_bits - actual number of bits to transmit
+//				  cmd_data - command data
+//				  addr_bits - actual number of bits to transmit
+//				  addr_data - address data
+//				  dout_bits - actual number of bits to transmit
+//				  dout_data - output data
+//				  din_bits - actual number of bits to receive
+//				  
+//		 Returns: read data - uint32 containing read in data only if RX was set 
+//				  0 - something went wrong (or actual read data was 0)
+//				  1 - data sent ok (or actual read data is 1)
+//				  Note: all data is assumed to be stored in the lower bits of
+//				  the data variables (for anything <32 bits). 
+//
+////////////////////////////////////////////////////////////////////////////////
+
+uint32 spi_transaction(uint8 spi_no, uint8 cmd_bits, uint16 cmd_data, uint32 addr_bits, uint32 addr_data, uint32 dout_bits, uint32 dout_data,
+				uint32 din_bits, uint32 dummy_bits){
+
+	if(spi_no > 1) return 0;  //Check for a valid SPI 
+
+	//code for custom Chip Select as GPIO PIN here
+
+	while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR); //wait for SPI to be ready	
+
+//########## Enable SPI Functions ##########//
+	//disable MOSI, MISO, ADDR, COMMAND, DUMMY in case previously set.
+	CLEAR_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_MOSI|SPI_USR_MISO|SPI_USR_COMMAND|SPI_USR_ADDR|SPI_USR_DUMMY);
+
+	//enable functions based on number of bits. 0 bits = disabled. 
+	//This is rather inefficient but allows for a very generic function.
+	//CMD ADDR and MOSI are set below to save on an extra if statement.
+//	if(cmd_bits) {SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_COMMAND);}
+//	if(addr_bits) {SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_ADDR);}
+	if(din_bits) {SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_MISO);}
+	if(dummy_bits) {SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_DUMMY);}
+//########## END SECTION ##########//
+
+//########## Setup Bitlengths ##########//
+	WRITE_PERI_REG(SPI_USER1(spi_no), ((addr_bits-1)&SPI_USR_ADDR_BITLEN)<<SPI_USR_ADDR_BITLEN_S | //Number of bits in Address
+									  ((dout_bits-1)&SPI_USR_MOSI_BITLEN)<<SPI_USR_MOSI_BITLEN_S | //Number of bits to Send
+									  ((din_bits-1)&SPI_USR_MISO_BITLEN)<<SPI_USR_MISO_BITLEN_S |  //Number of bits to receive
+									  ((dummy_bits-1)&SPI_USR_DUMMY_CYCLELEN)<<SPI_USR_DUMMY_CYCLELEN_S); //Number of Dummy bits to insert
+//########## END SECTION ##########//
+
+//########## Setup Command Data ##########//
+	if(cmd_bits) {
+		SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_COMMAND); //enable COMMAND function in SPI module
+		uint16 command = cmd_data << (16-cmd_bits); //align command data to high bits
+		command = ((command>>8)&0xff) | ((command<<8)&0xff00); //swap byte order
+		WRITE_PERI_REG(SPI_USER2(spi_no), ((((cmd_bits-1)&SPI_USR_COMMAND_BITLEN)<<SPI_USR_COMMAND_BITLEN_S) | command&SPI_USR_COMMAND_VALUE));	
+	}
+//########## END SECTION ##########//
+
+//########## Setup Address Data ##########//
+	if(addr_bits){
+		SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_ADDR); //enable ADDRess function in SPI module
+		WRITE_PERI_REG(SPI_ADDR(spi_no), addr_data<<(32-addr_bits)); //align address data to high bits
+	}
+	
+
+//########## END SECTION ##########//	
+
+//########## Setup DOUT data ##########//
+	if(dout_bits) {
+		SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_USR_MOSI); //enable MOSI function in SPI module
 	//copy data to W0
 	if(READ_PERI_REG(SPI_USER(spi_no))&SPI_WR_BYTE_ORDER) {
-		WRITE_PERI_REG(SPI_W0(spi_no), data<<(32-no_bits));
-	} else if(extra_bits){
-		//if your data isn't a byte multiple (8/16/24/32 bits)and you don't have SPI_WR_BYTE_ORDER set, you need this to move the non-8bit remainder to the MSBs
-		//not sure if there's even a use case for this, but it's here if you need it...
-		//for example, 0xDA4 12 bits without SPI_WR_BYTE_ORDER would usually be output as if it were 0x0DA4, 
-		//of which 0xA4, and then 0x0 would be shifted out (first 8 bits of low byte, then 4 MSB bits of high byte - ie reverse byte order). 
-		//The code below shifts it out as 0xA4 followed by 0xD as you might require. 
-		WRITE_PERI_REG(SPI_W0(spi_no), ((0xFFFFFFFF<<(no_bits - extra_bits)&data)<<(8-extra_bits) | (0xFFFFFFFF>>(32-(no_bits - extra_bits)))&data));
+		WRITE_PERI_REG(SPI_W0(spi_no), dout_data<<(32-dout_bits));
 	} else {
-		WRITE_PERI_REG(SPI_W0(spi_no), data);
+
+		uint8 dout_extra_bits = dout_bits%8;
+
+		if(dout_extra_bits){
+			//if your data isn't a byte multiple (8/16/24/32 bits)and you don't have SPI_WR_BYTE_ORDER set, you need this to move the non-8bit remainder to the MSBs
+			//not sure if there's even a use case for this, but it's here if you need it...
+			//for example, 0xDA4 12 bits without SPI_WR_BYTE_ORDER would usually be output as if it were 0x0DA4, 
+			//of which 0xA4, and then 0x0 would be shifted out (first 8 bits of low byte, then 4 MSB bits of high byte - ie reverse byte order). 
+			//The code below shifts it out as 0xA4 followed by 0xD as you might require. 
+			WRITE_PERI_REG(SPI_W0(spi_no), ((0xFFFFFFFF<<(dout_bits - dout_extra_bits)&dout_data)<<(8-dout_extra_bits) | (0xFFFFFFFF>>(32-(dout_bits - dout_extra_bits)))&dout_data));
+		} else {
+			WRITE_PERI_REG(SPI_W0(spi_no), dout_data);
+		}
 	}
-	//send
+	}
+//########## END SECTION ##########//
+
+//########## Begin SPI Transaction ##########//
 	SET_PERI_REG_MASK(SPI_CMD(spi_no), SPI_USR);
+//########## END SECTION ##########//
+
+//########## Return DIN data ##########//
+	if(din_bits) {
+		while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR);	//wait for SPI transaction to complete
+		
+		if(READ_PERI_REG(SPI_USER(spi_no))&SPI_RD_BYTE_ORDER) {
+			return READ_PERI_REG(SPI_W0(spi_no)) >> (32-din_bits); //Assuming data in is written to MSB. TBC
+		} else {
+			return READ_PERI_REG(SPI_W0(spi_no)); //Read in the same way as DOUT is sent. Note existing contents of SPI_W0 remain unless overwritten! 
+		}
+
+		return 0; //something went wrong
+	}
+//########## END SECTION ##########//
+
+	//Transaction completed
+	return 1; //success
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-
-/* Moved to spi.h as expansion macros
-////////////////////////////////////////////////////////////////////////////////
-//
-// Function Name: spi_tx8
-//   Description: SPI transmit 8bits of data
-//    Parameters: spi_no - SPI (0) or HSPI (1)
-//				  data - actual data to transmit
-//				 
-////////////////////////////////////////////////////////////////////////////////
-
-
-void spi_tx8(uint8 spi_no, uint8 data){
-
-	spi_txd(spi_no, 8, (uint32) data);
-
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////
-//
-// Function Name: spi_tx16
-//   Description: SPI transmit 16bits of data
-//    Parameters: spi_no - SPI (0) or HSPI (1)
-//				  data - actual data to transmit
-//				 
-////////////////////////////////////////////////////////////////////////////////
-
-void spi_tx16(uint8 spi_no, uint16 data){
-
-	spi_txd(spi_no, 16, (uint32) data);
-
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////
-//
-// Function Name: spi_tx32
-//   Description: SPI transmit 32bits of data
-//    Parameters: spi_no - SPI (0) or HSPI (1)
-//				  data - actual data to transmit
-//				 
-////////////////////////////////////////////////////////////////////////////////
-
-void spi_tx32(uint8 spi_no, uint32 data){
-
-	spi_txd(spi_no, 32, data);
-
-}
-
-////////////////////////////////////////////////////////////////////////////////
-*/
-
-//TODO spi_rxd function
 
 /*///////////////////////////////////////////////////////////////////////////////
 //
--- include/driver/spi.h
+++ include/driver/spi.h
@@ -51,20 +51,26 @@
 #define SPI_CLK_FREQ CPU_CLK_FREQ/(SPI_CLK_PREDIV*SPI_CLK_CNTDIV) // 80 / 20 = 4 MHz
 
 
-#define spi_tx8(spi_no, data) spi_txd(spi_no, 8, (uint32) data)
-#define spi_tx16(spi_no, data) spi_txd(spi_no, 16, (uint32) data)
-#define spi_tx32(spi_no, data) spi_txd(spi_no, 32, (uint32) data)
+
 
 
 void spi_init(uint8 spi_no);
-
 void spi_init_gpio(uint8 spi_no, uint8 sysclk_as_spiclk);
-
 void spi_clock(uint8 spi_no, uint16 prediv, uint8 cntdiv);
-
 void spi_tx_byte_order(uint8 spi_no, uint8 byte_order);
+void spi_rx_byte_order(uint8 spi_no, uint8 byte_order);
+uint32 spi_transaction(uint8 spi_no, uint8 cmd_bits, uint16 cmd_data, uint32 addr_bits, uint32 addr_data, uint32 dout_bits, uint32 dout_data, uint32 din_bits, uint32 dummy_bits);
 
-//TODO add all function descriptors in here
+//Expansion Macros
+#define spi_txd(spi_no, bits, data) spi_transaction(spi_no, 0, 0, 0, 0, bits, (uint32) data, 0, 0)
+#define spi_tx8(spi_no, data)       spi_transaction(spi_no, 0, 0, 0, 0, 8,    (uint32) data, 0, 0)
+#define spi_tx16(spi_no, data)      spi_transaction(spi_no, 0, 0, 0, 0, 16,   (uint32) data, 0, 0)
+#define spi_tx32(spi_no, data)      spi_transaction(spi_no, 0, 0, 0, 0, 32,   (uint32) data, 0, 0)
+
+#define spi_rxd(spi_no, bits) spi_transaction(spi_no, 0, 0, 0, 0, 0, 0, bits, 0)
+#define spi_rx8(spi_no)       spi_transaction(spi_no, 0, 0, 0, 0, 0, 0, 8,    0)
+#define spi_rx16(spi_no)      spi_transaction(spi_no, 0, 0, 0, 0, 0, 0, 16,   0)
+#define spi_rx32(spi_no)      spi_transaction(spi_no, 0, 0, 0, 0, 0, 0, 32,   0)
 
 #endif
 
